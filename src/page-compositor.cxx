922071fb40a544586535985aed78a60008a2f364
Benoit Gschwind <gschwind@gnu-log.net>
2016-06-04 22:31:54 +0200

    durty

--- src/page-compositor.cxx
+++ src/page-compositor.cxx
@@ -17,80 +17,17 @@
  * along with page-compositor.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 #include "config.hxx"
 
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-#include <libweston-0/compositor.h>
-#include <libweston-0/compositor-x11.h>
+#include "compositor.hxx"
 
-
-using backend_init_func =
-		int (*)(struct weston_compositor *c,
-	    int *argc, char *argv[],
-	    struct weston_config *config,
-	    struct weston_backend_config *config_base);
-
-void load_x11_backend(weston_compositor* ec) {
-	weston_x11_backend_config config = {{ 0, }};
-	weston_x11_backend_output_config default_output = { 0, };
-
-	default_output.height = 600;
-	default_output.width = 800;
-	default_output.name = strdup("Wayland output");
-	default_output.scale = 1;
-	default_output.transform = WL_OUTPUT_TRANSFORM_NORMAL;
-
-	config.base.struct_size = sizeof(weston_x11_backend_config);
-	config.base.struct_version = WESTON_X11_BACKEND_CONFIG_VERSION;
-
-	config.fullscreen = 0;
-	config.no_input = 0;
-	config.num_outputs = 1;
-	config.outputs = &default_output;
-	config.use_pixman = 0;
-
-	auto backend_init = reinterpret_cast<backend_init_func>(
-			weston_load_module("x11-backend.so", "backend_init"));
-	if (!backend_init)
-		return;
-
-	backend_init(ec, NULL, NULL, NULL, &config.base);
-
-	free(default_output.name);
-
-}
+/* TODO: better global */
+compositor_t* cmp{nullptr};
 
 int main(int argc, char** argv) {
-
-    weston_log_set_handler(vprintf, vprintf);
-
-	/* first create the wayland serveur */
-	auto dpy = wl_display_create();
-
-	auto sock_name = wl_display_add_socket_auto(dpy);
-	weston_log("socket name = %s\n", sock_name);
-
-	/* set the environment for children */
-	setenv("WAYLAND_DISPLAY", sock_name, 1);
-
-	auto ec = weston_compositor_create(dpy, nullptr);
-
-	/* setup the keyboard layout (MANDATORY) */
-	xkb_rule_names names = {
-			"",					/*rules*/
-			"pc104",			/*model*/
-			"us",				/*layout*/
-			"",					/*variant*/
-			""					/*option*/
-	};
-	weston_compositor_set_xkb_rule_names(ec, &names);
-	load_x11_backend(ec);
-
-	weston_compositor_wake(ec);
-    wl_display_run(dpy);
-
+	cmp = new compositor_t();
+	cmp->run();
 	return EXIT_SUCCESS;
 }
 
