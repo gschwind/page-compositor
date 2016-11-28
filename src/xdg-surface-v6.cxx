699d22449c489c953f50b129cd9964c554e11a67
Benoit Gschwind <gschwind@gnu-log.net>
2016-11-28 17:37:16 +0100

    fix invalid xdg_shell_v6 resource creation

--- src/xdg-surface-v6.cxx
+++ src/xdg-surface-v6.cxx
@@ -28,7 +28,7 @@ xdg_surface_v6_t::xdg_surface_v6_t(
 	
 	/* allocate a wayland resource for the provided 'id' */
 	_resource = wl_resource_create(client,
-			reinterpret_cast<wl_interface const *>(&zxdg_shell_v6_interface), 1, id);
+			reinterpret_cast<wl_interface const *>(&zxdg_surface_v6_interface), 1, id);
 
 	/**
 	 * Define the implementation of the resource and the user_data,
